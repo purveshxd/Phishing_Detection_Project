fea1a7389189c0c3f56d5b19a52798b601614083
Purvesh Dongarwar <97460224+purveshxd@users.noreply.github.com>
2022-04-14 20:21:15 +0530

    Update MiniProject_Web.py

--- MiniProject_Web.py
+++ MiniProject_Web.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import pickle
 import streamlit as st
-import sklearn
-import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, accuracy_score, f1_score
 
