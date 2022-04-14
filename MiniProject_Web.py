7fd430dd066dbce7ac189ae6935fd957c5e1cba4
Purvesh Dongarwar <97460224+purveshxd@users.noreply.github.com>
2022-04-14 20:04:17 +0530

    Add files via upload

---
+++ MiniProject_Web.py
@@ -0,0 +1,39 @@
+from email import header
+from multiprocessing.sharedctypes import Value
+from turtle import color
+import click
+from idna import valid_contextj
+import pandas as pd
+import numpy as np
+import pickle
+import streamlit as st
+import sklearn
+import os
+# import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, accuracy_score, f1_score
+
+loaded_model = pickle.load(open('D:/Dev/MiniProject/phishing.pkl','rb'))
+
+st.title('Phishing Detection')
+
+st.header('Mini Project')
+
+predict_input = st.text_input("Enter link below","") 
+predict_input_raw = [predict_input]
+
+# predict_bad = ['yeniik.com.tr/wp-admin/js/login.alibaba.com/login.jsp.php']
+# predict_good = ['youtube.com/']
+result = loaded_model.predict(predict_input_raw)
+
+
+print(result)
+print("*"*30)
+if st.button("check"):
+    if result==['bad']:
+        st.header('Phishing')
+    if result==['good']:
+        st.header('Not Phishing')
+    
+
+    
